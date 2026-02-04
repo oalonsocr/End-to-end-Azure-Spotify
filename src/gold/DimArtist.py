6ab0ee508fb49d3cf917728415af4e188b939e10
oalonsocr <143976220+oalonsocr@users.noreply.github.com>
2026-02-04 03:45:16 -0500

    Add files via upload

---
+++ src/gold/DimArtist.py
@@ -0,0 +1,20 @@
+import dlt
+
+@dlt.table
+def dimartistStg():
+    df = spark.readStream.table("spotify_cata.silver.dimartist")
+    return df
+
+
+dlt.create_streaming_table("dimartist")
+
+dlt.create_auto_cdc_flow(
+    target= "dimartist",
+    source= "dimartistStg",
+    keys= ["artist_id"],
+    sequence_by= "updated_at",
+    stored_as_scd_type= 2,
+    track_history_except_column_list= None,
+    name= None,
+    once= False
+)
